b9a031888b0e54521567da7d73823792e1d2c0c4
Valeriano-WebDev <Valeriano-WebDev@gmail.com>
2022-02-23 11:12:15 -0800

    Refactored spacing

--- func-loops-arrays-kat-jacob.js
+++ func-loops-arrays-kat-jacob.js
@@ -77,31 +77,43 @@ console.log(findHighest(indexHighestNumber))
 //STRETCH Challenges
 //Challenge #1
 //Create a function that takes in two arrays and returns one array with no duplicate values.
-//Psuedo Code
-
-
-
 var arr1 = [3, 7, 10, 5, 4, 3, 3]
 var arr2 = [7, 8, 2, 3, 1, 5, 4]
 // --> [3, 7, 10, 5, 4, 8, 2, 1]
 
-const arr3 = (array1 , array2) => {
-    let combinedArr = []
-    for (let i = 0; i < array1.length; i++) {
-        if(combinedArr[i] % array2[i] !== 0 || combinedArr[i] % array1[i] !== 0) {
-            combinedArr.push(array2[i] || array1[i])
 
-        }
+//Challenge #2
+//Create a function that takes in two numbers as arguments and returns an array the length of the first number filled with the second number.
+var arrayLength = 6
+var arrayValue = 0
+// --> [0, 0, 0, 0, 0, 0]
 
+var arrayLength = 4
+var arrayValue = 11
+// --> [11, 11, 11, 11]
+
+
+
+//Challenge #3
+//Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function.
+var addUp1 = 4
+// 1 + 2 + 3 + 4 = 10
+// --> 10
+
+var addUp2 = 10
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
+// --> 55
+
+var addUp3 = 600
+// --> 180300
+
+const addAll = (num) => {
+    let counter = 0
+    for(let i = 1; i <= num; i++){
+        counter += i
     }
-    return combinedArr
+    return counter
 }
-console.log(arr3(arr1, arr2))
-
-//
-// for(let j = 0; j < superman.length; j++){
-//     if(superman[i] !== batman[j]) {
-//         combinedArr.push(array[i])
-//     }
-//
-// }
+console.log(addAll(addUp1))
+console.log(addAll(addUp2))
+console.log(addAll(addUp3))
