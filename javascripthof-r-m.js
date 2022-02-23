85dcf34235ec6cf625838c101450fc9e645d8983
RyanJ2463 <jacobellr2017@gmail.comjacobellr2017@gmail.comcd>
2022-02-23 15:50:41 -0500

    higher order functions

--- javascripthof-r-m.js
+++ javascripthof-r-m.js
@@ -6,11 +6,11 @@
 //multiplied by 10 all the values 
 //output // --> [30, 90, 150, 40, 100]
 
- var arr1 = [3, 9, 15, 4, 10]
- const multiplied = arr1.map(value => {
-     return value * 10
- })
- console.log(multiplied);
+//  var arr1 = [3, 9, 15, 4, 10]
+//  const multiplied = arr1.map(value => {
+//      return value * 10
+//  })
+//  console.log(multiplied);
 
 
 
@@ -22,23 +22,57 @@
 //filter through to get only odd numbers
 // output --> [7, 3, 5, 13]
 
- var arr2 = [2, 7, 3, 5, 8, 10, 13]
- const onlyOdd = arr2.filter(value => {
-     return value % 2 !==0
- })
-console.log(onlyOdd);
+//  var arr2 = [2, 7, 3, 5, 8, 10, 13]
+//  const onlyOdd = arr2.filter(value => {
+//      return value % 2 !==0
+//  })
+// console.log(onlyOdd);
 
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
- var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
 
 
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+//  comboArr.forEach((value) => {
+//      console.log(typeof value === "string")
 
+//  })
+
+// 
+// const strFinder = (array) => {
+//     let filterArray = array.filter(value => {
+//         return typeof value === "string"
+//     }) 
+//     return filterArray.join("")
+// }
+
+// console.log(strFinder(comboArr))
 // // --> "nicework"
 
 
 
 
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-// var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
-// // --> [58, "abcd", true]
+var filterArrayValues = [58, " ", "abcd", true, null, false, 0]
+// --> [58, "abcd", true]
+
+// create a function filteredArray 
+// use higher order function .filter through array without any false, null, 0, or blank values
+// return the array 
+
+
+
+const filteredArray = (array) => {
+    let filtered = array.filter(value => {
+        if (value !== false && value !== null && value !==0 && value !== " "){
+            return value 
+        }
+    })
+    return filtered
+    }
+console.log(filteredArray(filterArrayValues))
+
+
+
+    
+
