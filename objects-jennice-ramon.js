3977e22aa9e5065077fc8e01244c40f23ee57260
Ramon Bolds <ramonbolds98@gmail.com>
2022-02-24 17:50:48 -0600

    finish

--- objects-jennice-ramon.js
+++ objects-jennice-ramon.js
@@ -39,11 +39,11 @@
 // console.log(describeProduct(product))
 
 // Consider this variable:
- var lunch = {
-   name: "PB and Banana",
-   type: "sandwich",
-   ingredients: ["bread", "peanut butter", "banana"]
-}
+ //var lunch = {
+ //  name: "PB and Banana",
+  // type: "sandwich",
+   //ingredients: ["bread", "peanut butter", "banana"]
+//}
 
 // Write the code that accesses the ingredients property.
 //run console log that accesses the lunch object with . notation for the ingredients key values
@@ -58,25 +58,25 @@
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
 //Create a function that takes lunch object and returns a string that says "the ingredients for a PB and banana sandwich are bread, peanut butter, and banana"
 //
-const mealTime = (object) => {
-    return `The ingredients for a ${object.name} ${object.type} are ${object.ingredients[0]}, ${object.ingredients[1]}, and ${object.ingredients[2]}.`
- }
+//const mealTime = (object) => {
+  //  return `The ingredients for a ${object.name} ${object.type} are $///////{object.ingredients[0]}, ${object.ingredients[1]}, and ${object.///////ingredients[2]}.`
+ //}
 //
 // console.log(mealTime(lunch))
 
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
 
-lunch.string = mealTime
+//lunch.string = mealTime
 
- console.log()
+ //console.log()
 
 // Consider this variable:
-var animals = [
-  { name: "Waffles", type: "dog", age: 12 },
-  { name: "Fluffy", type: "cat", age: 14 },
-  { name: "Spelunky", type: "dog", age: 4 },
-  { name: "Hank", type: "cat", age: 11 },
-]
+//var animals = [
+  //{ name: "Waffles", type: "dog", age: 12 },
+  //{ name: "Fluffy", type: "cat", age: 14 },
+  //{ name: "Spelunky", type: "dog", age: 4 },
+  //{ name: "Hank", type: "cat", age: 11 },
+//]
 
 // Create a function that takes in any array of objects and returns a new array with only those of type cat.
 //pseudo code:
@@ -86,33 +86,52 @@ var animals = [
 // If cat then adds to new array,
 // If not cat then ignores
 
-const catsOnly = (array){
-  array.filter(animals[1] == "cat")  
-}
-
+// const catsOnly = (array)=>{
+  
+//   return array.filter(value => value.type == "cat")  
+// }
+// console.log(catsOnly(animals))
 
 
 // Using the same array of objects above. Create a function that returns a new array with only the names of the animals.
-// Consider this variable:
-// let author = {
-//     name: "H. G. Wells",
-//     genre: "science fiction"
+// //create a function that takes an array iterate thru array and returns name of animals
+// const nameOnly = (array)=>{
+//   return array.map(value=>{
+//     return value.name
+//   })
 // }
+// console.log(nameOnly(animals))
+// Consider this variable:
+let author = {
+  name: "H. G. Wells",
+  genre: "science fiction"
+}
 // Write the code that destructures the author object and makes the following work:
-// console.log(`${name} is a ${genre} author`)
+//assign a variable for name and variable for genre
+//write the code that access the key value for name from object
+//write the code that access the key value for genre from object
+const name = author.name 
+const genre = author.genre
+
+//console.log(`${name} is a ${genre} author`)
 // --> "H. G. Wells is a science fiction author"
 // Consider this variable:
-// let pokeOne = {
-//     species: "Charmandar",
-//     pokemon_type: "Fire"
-// }
-//
-// let pokeTwo = {
-//     species: "Magikarp",
-//     pokemon_type: "Water"
-// }
+let pokeOne = {
+  species: "Charmandar",
+  pokemon_type: "Fire"
+}
+
+let pokeTwo = {
+  species: "Magikarp",
+  pokemon_type: "Water"
+}
+
 // Write a function called describePokemon() that take an object like the ones above and uses destructuring to return a description of the Pokemon such that:
-// console.log(describePokemon(pokeOne))
-// --> "Charmandar is a Fire pokemon"
-// console.log(describePokemon(pokeTwo))
-// --> "Magikarp is a Water pokemon"
+var describePokemon = (object)=>{
+return `${object.species} is a ${object.pokemon_type} pokemon`
+}
+
+console.log(describePokemon(pokeOne))
+//--> "Charmandar is a Fire pokemon"
+console.log(describePokemon(pokeTwo))
+//--> "Magikarp is a Water pokemon"
