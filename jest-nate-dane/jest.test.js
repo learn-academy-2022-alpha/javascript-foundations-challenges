ae7d1015783ca209b6f777936a975e09a2fc8722
Dane Roelofs <dane.l.roelofs@gmail.com>
2022-02-23 16:40:35 -0800

    finished the challenges

--- jest-nate-dane/jest.test.js
+++ jest-nate-dane/jest.test.js
@@ -116,37 +116,120 @@ const fruit = (type) => {
 
 // Write the test for a function called rick that returns "Morty".
 
+describe("rick", () => {
+    it("returns Morty", () => {
+        expect(rick()).toEqual("Morty")
+    })
+})
 
 // Write the function that will make the test pass.
 
+const rick = () => "Morty"
 
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
 
+describe("greeter", () => {
+    it("returns a greeting with that name to the screen", () => {
+        expect(greeter("Bob")).toEqual("Hello, Bob")
+        expect(greeter("Rick")).toEqual("Hello, Rick")
+    })
+})
 
 // Write the function that will make the test pass.
 
+const greeter = name => `Hello, ${name}`
 
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
+
+describe("oddOrEven", () => {
+    it("returns whether the number is odd or even", () => {
+        expect(oddOrEven(20)).toEqual("even")
+        expect(oddOrEven(21)).toEqual("odd")
+    })
+})
+
 // Write the function that will make the test pass.
 
+const oddOrEven = num => {
+    if (num % 2 === 0) {
+        console.log("even")
+        return "even"
+    }
+    else {
+        console.log("odd")
+        return "odd"
+    }
+}
 
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
+
+describe("doubler", () => {
+    it("returns the result of the number multiplied by 2", () => {
+        expect(doubler(20)).toEqual(40)
+        expect(doubler(21)).toEqual(42)
+    })
+})
+
 // Write the function that will make the test pass.
 
+const doubler = num => num * 2
 
 // Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
 
+describe("multiply", () => {
+    it("returns the result of one of the numbers multiplied by the other", () => {
+        expect(multiply(5, 5)).toEqual(25)
+    })
+})
 
 // Write the function that will make the test pass.
 
+const multiply = (num1, num2) => num1 * num2
 
 // Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
+describe("divisibleBy", () => {
+    it("returns whether the first number is evenly divisible by the second", () => {
+        expect(divisibleBy(10, 5)).toEqual("10 is evenly divisible by 5")
+        expect(divisibleBy(12, 5)).toEqual("12 is not evenly divisible by 5")
+    })
+})
 
 // Write the function that will make the test pass.
+
+const divisibleBy = (num1, num2) =>  {
+    if (num1 % num2 === 0) {
+        return `${num1} is evenly divisible by ${num2}`
+    } else {
+        return `${num1} is not evenly divisible by ${num2}`
+    }
+}
 
 
 // Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'.
 
+describe("fizzbuzz", () => {
+    it("If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'", () => {
+        expect(fizzbuzz(3)).toEqual("fizz")
+        expect(fizzbuzz(5)).toEqual("buzz")
+        expect(fizzbuzz(15)).toEqual("fizzbuzz")
+        expect(fizzbuzz(7)).toEqual(7)
+    })
+})
 
 // Write the function that will make the test pass.
+
+const fizzbuzz = num => {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "fizzbuzz"
+    }
+    else if (num % 3 === 0) {
+        return "fizz"
+    }
+    else if (num % 5 === 0) {
+        return "buzz"
+    }
+    else {
+        return num
+    }
+}
