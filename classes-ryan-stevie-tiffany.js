fa8757116d9eed84b0888e1bfeafd5b78e3637c8
RyanJ2463 <jacobellr2017@gmail.comjacobellr2017@gmail.comcd>
2022-02-28 14:38:54 -0500

    2nd question done

--- classes-ryan-stevie-tiffany.js
+++ classes-ryan-stevie-tiffany.js
@@ -36,23 +36,73 @@ class Coffee {
   }
 }
 // Write the code that makes a black coffee. Then write the code that outputs the coffee's profile.
-const blackcoffee = new Coffee("black", 0, 0)
-console.log(blackcoffee.coffeeProfile())
+// const blackcoffee = new Coffee("black", 0, 0)
+// console.log(blackcoffee.coffeeProfile())
 
 // Write the code that makes a coffee with 1 cream and 2 sugars. Then write the code that outputs the coffee's profile.
-const otherCoffee = new Coffee("normal", 1, 2)
-console.log(otherCoffee.coffeeProfile())
+// const otherCoffee = new Coffee("normal", 1, 2)
+// console.log(otherCoffee.coffeeProfile())
+
+
+
+
+class Latte {
+  constructor(flavor, milk, shot){
+    this.flavor = flavor.toLowerCase()
+    this.milk = milk
+    this.shot = shot
+  }
+
+  latteProfile(){
+    return `A ${this.flavor} latte with ${this.milks()}, ${this.shots()}`
+  }
+
+  milks(){
+    if(this.milk === "almond"){
+      return `${this.milk} milks`
+    } else {
+      return `${this.milk} milk`
+    }
+  }
+
+  shots(){
+    if(this.shot > 1){
+      return `${this.shot} shots`
+    } else {
+      return `${this.shot} shot`
+    }
+  }
+
+  flavor() {
+      if(this.flavor === "vanilla") {
+          return `${this.flavor}`
+      } else if (this.flavor === "regular"){
+        return "regular"
+      } else {
+        return "hazelnut"
+      }
+  }
+}
+
+
+
+
 
 
 
 // Latte Maker
 // Write a Latte class that takes a flavor, a milk type and a number of shots.
-
+const regLatte = new Latte("vanilla", 2, 3)
+console.log(regLatte.latteProfile())
 // Write a method for your Latte class that outputs the latte's profile.
+console.log(regLatte.latteProfile())
 
 // Write the code that makes a regular, single shot latte. Then, log the latte's profile.
-
+const goodLatte = new Latte("regular", 0, 1)
+console.log(goodLatte.latteProfile())
 // Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
+const hazLatte = new Latte ("hazelnut", "almond", 2)
+console.log(hazLatte.latteProfile());
 
 
 
