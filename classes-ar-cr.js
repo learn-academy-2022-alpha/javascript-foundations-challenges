3c58fef56f16845a7b400313577566667f872165
Alex Ranga <alexisranga@gmail.com>
2022-02-28 13:32:45 -0800

    completed classes challenge

--- classes-ar-cr.js
+++ classes-ar-cr.js
@@ -1,33 +1,31 @@
+// 1. Coffee Maker
 class Coffee {
-    constructor(type, cream, sugar){
-      this.type = type.toLowerCase()
-      this.cream = cream
-      this.sugar = sugar
-    }
-  
-    coffeeProfile(){
-      return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
-    }
-  
-    creams(){
-      if(this.cream > 1){
-        return `${this.cream} creams`
-      } else {
-        return `${this.cream} cream`
-      }
-    }
-  
-    sugars(){
-      if(this.sugar > 1){
-        return `${this.sugar} sugars`
-      } else {
-        return `${this.sugar} sugar`
-      }
+  constructor(type, cream, sugar){
+    this.type = type.toLowerCase()
+    this.cream = cream
+    this.sugar = sugar
+  }
 
-     
+  coffeeProfile(){
+    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+  }
+
+  creams(){
+    if(this.cream > 1){
+      return `${this.cream} creams`
+    } else {
+      return `${this.cream} cream`
     }
   }
 
+  sugars(){
+    if(this.sugar > 1){
+      return `${this.sugar} sugars`
+    } else {
+      return `${this.sugar} sugar`
+    }
+  }
+}
   // Write the code that makes a black coffee. Then write the code that outputs the coffee's profile.
 const blackCoffee = new Coffee("black", 0, 0)
 console.log(blackCoffee)
@@ -38,6 +36,14 @@ console.log(blackCoffee.coffeeProfile())
 const exSweet = new Coffee ("Extra Sweet", 1, 2)
 console.log(exSweet.coffeeProfile())
 
+// 2. Latte Maker
+// Write a Latte class that takes a flavor, a milk type and a number of shots.
+
+// Write a method for your Latte class that outputs the latte's profile.
+
+// Write the code that makes a regular, single shot latte. Then, log the latte's profile.
+
+// Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
 
 class Latte {
     constructor(flavor, milk, shots){
@@ -58,17 +64,22 @@ const doubleShot = new Latte ("hazelnut", "almond", 2)
 // console.log(singleShot.latteProfile())
 console.log(doubleShot.latteProfile())
 
-// Write a Latte class that takes a flavor, a milk type and a number of shots.
-
-// Write a method for your Latte class that outputs the latte's profile.
-
-// Write the code that makes a regular, single shot latte. Then, log the latte's profile.
-
-
-// Write the code that makes a double shot hazelnut latte with almond milk. Then, log the latte's profile.
-
 
 
 // Volume of a Cylinder
 // Write a class that calculates the volume of a Cylinder to four decimal places. Volume of a cylinder : V = πr2h (r is the radius and h is the height of the cylinder)
 
+class CylinderVolume {
+  constructor(radius, height){
+    this.pie = 3.14159
+    this.radius = radius ** 2
+    this.height = height
+  }
+
+  volume(){
+    return `The volume of a cylinder is ${this.pie * this.radius * this.height}.`
+  }
+
+}
+const aCylinder = new CylinderVolume(5, 6)
+console.log(aCylinder.volume())
